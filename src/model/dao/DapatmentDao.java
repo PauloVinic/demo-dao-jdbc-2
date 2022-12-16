e4ac7b5f0897469e6b79b4ce5a6996105f03ebed
Paulo <paulo_vinicius_21@hotmail.com>
2022-12-16 09:53:31 -0300

    SellerDaoJDBC and DaoFactory

--- src/model/dao/DapatmentDao.java
+++ src/model/dao/DapatmentDao.java
@@ -5,11 +5,10 @@ import java.util.List;
 import model.entities.Department;
 
 public interface DapatmentDao {
+	
 	void insert(Department obj);
 	void update(Department obj);
 	void deleteById(Integer Id);
 	Department findById(Integer id);
 	List<Department> findAll();
-	
-
 }
